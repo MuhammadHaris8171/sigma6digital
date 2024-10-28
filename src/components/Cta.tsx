9f818140d74f65b6ac47a7afc782fd9743e31c6e
Muhammad Rameez <m.rameezwork@gmail.com>
2024-10-28 05:45:09 -0700

    some media query added on homepage

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,27 +1,74 @@
-import styles from "./Cta.module.css"
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import styles from "./Cta.module.css";
 
 function Cta() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const mediaQuery = window.matchMedia("(max-width: 575px)");
+
+    const animateVideoContainer = () => {
+      const startPoint = mediaQuery.matches ? "top 50%" : "top 80%"; 
+      const heightValue = mediaQuery.matches ? "80svh" : "100svh";
+
+      gsap.to(`.${styles.videoContainer}`, {
+        scrollTrigger: {
+          trigger: `.${styles.videoContainer}`,
+          start: startPoint,
+          end: "top 35%",
+          scrub: true,
+          toggleActions: "play none none none",
+        },
+        width: "100%",
+        height: heightValue,
+        margin: 0,
+        duration: 1,
+        ease: "power2.out",
+        borderRadius: "0px",
+      });
+    };
+
+    animateVideoContainer();
+
+    window.addEventListener("resize", animateVideoContainer);
+
+    return () => {
+      window.removeEventListener("resize", animateVideoContainer);
+    };
+  }, []);
+
   return (
     <>
-    <div className={`${styles.videoContainer} position-relative`}>
-        <div className="contianer-fluid">
-            <video className={`${styles.ctaVideo}`} autoPlay muted loop>
-                <source src='https://sigma6digital.com/wp-content/uploads/2024/10/02-1.m4v' />
-            </video>
-            <div className={`${styles.videoContentContainer} d-flex flex-column align-items-center gap-3`}>
-                <h2 className={`${styles.ctaVideoHeading}`}>Begin your journey with us today.</h2>
-                <button className={`btn ${styles.ctaBtn} overflow-hidden`}><span>Get Started Now</span></button>
-            </div>
+      <div className={`${styles.videoContainer} position-relative`}>
+        <div className="container-fluid m-0 p-0">
+          <video className={`${styles.ctaVideo}`} autoPlay muted loop>
+            <source src="https://sigma6digital.com/wp-content/uploads/2024/10/02-1.m4v" />
+          </video>
+          <div
+            className={`${styles.videoContentContainer} d-flex flex-column align-items-center gap-3`}
+          >
+            <h2 className={`${styles.ctaVideoHeading}`}>
+              Begin your journey with us today.
+            </h2>
+            <button className={`btn ${styles.ctaBtn} overflow-hidden`}>
+              <span>Get Started Now</span>
+            </button>
+          </div>
         </div>
-    </div>
-    <div className={`${styles.ctaContentBox}`}>
-      <div className="container d-flex flex-column align-items-center justify-content-center gap-3">
-      <h3 className={`${styles.ctaContentBoxHeading}`}>Digitize Your Business for Success!</h3>
-      <button className={`btn ${styles.ctaBtn} overflow-hidden`}><span>Contact Us Now</span></button>
       </div>
-    </div>
+      <div className={`${styles.ctaContentBox}`}>
+        <div className="container d-flex flex-column align-items-center justify-content-center gap-3">
+          <h3 className={`${styles.ctaContentBoxHeading}`}>
+            Digitize Your Business for Success!
+          </h3>
+          <button className={`btn ${styles.ctaBtn} overflow-hidden`}>
+            <span>Contact Us Now</span>
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Cta
+export default Cta;
